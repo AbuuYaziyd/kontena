2d1d7ccdc7d3a64034c768b8986f30d95dcd1f6a
abouyaziyd <gakibrush@gmail.com>
2024-05-04 21:25:56 +0300

    home info

--- app/Views/layouts/info.php
+++ app/Views/layouts/info.php
@@ -7,12 +7,10 @@ $data = new Data();
 $knt = new Kontena();
 
 $kt = $knt->where('status', 1)->findAll();
-// $kontena = $knt->where('status', 1)->first();
 
 ?>
 
 <?php foreach ($kt as $dt) : ?>
-    
     <div class="col-12">
         <h1><span class=""><b><?= $dt['title'] ?></b></span>
             <span class="float-right"><b><?= $dt['year'] ?></b></span>
@@ -23,7 +21,7 @@ $kt = $knt->where('status', 1)->findAll();
                     <div class="inner">
                         <h3><?= $data->wanaohitajia($dt['id']) ?><sub style="font-size: 20px"> <i class="fas fa-paper-plane"></i></sub>
                         </h3>
-                        <p>Wanaohitajia</p>
+                        <p>Zinazohitajika</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-box"></i>
@@ -47,8 +45,7 @@ $kt = $knt->where('status', 1)->findAll();
                 <div class="small-box bg-<?= $dt['count'] <= $data->boxZote($dt['id']) ? 'danger' : 'warning' ?>" data-toggle="modal" data-target="#idadi">
                     <div class="inner">
                         <h3><?= $dt['count'] ?><sub style="font-size: 20px"> <i class="fas fa-shipping-fast"></i></sub></h3>
-
-                        <p>Idadi ya Boxi</p>
+                        <p>Idadi ya Boxi zote</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-parachute-box"></i>
@@ -59,8 +56,7 @@ $kt = $knt->where('status', 1)->findAll();
                 <div class="small-box bg-<?= $dt['count'] <= $data->boxZote($dt['id']) ? 'danger' : 'primary' ?>">
                     <div class="inner">
                         <h3><?= $dt['count'] - $data->boxZote($dt['id']) ?><sub style="font-size: 20px"> <i class="fas fa-boxes"></i></sub></h3>
-
-                        <p>Nafasi (Makadirio)</p>
+                        <p>Nafasi zilizobaki</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-people-carry"></i>
