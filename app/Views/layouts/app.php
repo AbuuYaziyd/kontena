57e8676105a000f8fbf78516d7c7c4c19fb0be8b
Abou Yaziyd <gakibrush@gmail.com>
2026-09-12 20:52:05 +0300

    update

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -18,12 +18,10 @@
     <link href="<?= base_url('app-assets/css/tabler-payments' . (session('lang') != 'ar' ? '' : '.rtl') . '.min.css') ?>" rel="stylesheet" />
     <link href="<?= base_url('app-assets/css/tabler-vendors' . (session('lang') != 'ar' ? '' : '.rtl') . '.min.css') ?>" rel="stylesheet" />
     <link href="<?= base_url('app-assets/css/demo' . (session('lang') != 'ar' ? '' : '.rtl') . '.min.css') ?>" rel="stylesheet" />
-    <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css') ?>">
     <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <?= $this->renderSection('styles') ?>
     <style>
-        @import url('https://rsms.me/inter/inter.css');
 
         :root {
             --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
